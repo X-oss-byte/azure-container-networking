4da84e2fd8f6b29540ae230c09794d63a2989550
camrynl <31013536+camrynl@users.noreply.github.com>
2023-08-03 13:46:02 -0700

    netperf script to print pod values

--- hack/scripts/netperf.sh
+++ hack/scripts/netperf.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 # find the nodes with netperf pods and assign test vars
+node_found=0
 for node in $(kubectl get nodes -o name);
 do
-    echo "Current : $node"
-    node_name="${node##*/}"
-    echo "checking whether the node has any netperf pods deployed to it"
-    pod_count=$(kubectl get pods -l app=container6 -o wide | grep "$node_name" -c)
-    netperf_pod=$(kubectl get pods -l app=container6 -o wide | grep "$node_name" | awk '{print $1}')
-    echo "netperf pod : $netperf_pod"
-    echo "pod_count: $pod_count"
+    while [ $node_found -lt 3 ]
+    do 
+        echo "Current : $node"
+        node_name="${node##*/}"
+        echo "checking whether the node has any netperf pods deployed to it"
+        pod_count=$(kubectl get pods -l app=container6 -o wide | grep "$node_name" -c)
+        netperf_pod=$(kubectl get pods -l app=container6 -o wide | grep "$node_name" | awk '{print $1}')
+        echo "netperf pod : $netperf_pod"
+        echo "pod_count: $pod_count"
 
-    if [ $pod_count -gt 1 ]; then 
-        target_pod=$(echo $netperf_pod | cut -d" " -f 1)
-        target_pod_ip=$(kubectl get pod "$target_pod" -o jsonpath='{.status.podIP}')
-        same_vm_pod=$(echo $netperf_pod | cut -d" " -f 2)
-        kubectl exec -it $target_pod -- netserver
-    else
-        diff_vm_pod=$netperf_pod
-    fi
+        if [ $pod_count -gt 1 ]; then 
+            target_pod=$(echo $netperf_pod | cut -d" " -f 1)
+            target_pod_ip=$(kubectl get pod "$target_pod" -o jsonpath='{.status.podIP}')
+            same_vm_pod=$(echo $netperf_pod | cut -d" " -f 2)
+            kubectl exec -it $target_pod -- netserver
+            node_found=$((node_found + 1))
+            echo "Number of nodes found with netperf pod: $node_found"
+        else
+            diff_vm_pod=$netperf_pod
+            node_found=$((node_found + 1))
+            echo "Number of nodes found with netperf pod: $node_found"
+        fi
+    done
 done
+
+echo "target netperf pod: $target_pod"
+echo "target netperf pod IP: $target_pod_ip"
+echo "same vm pod: $same_vm_pod"
+echo "different vm pod: $diff_vm_pod"
 
 #netperf on same vm pod
 iteration=10
